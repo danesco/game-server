6e46c10830ed7c120cc14d1975420673a37d9b4e
Dan Escobedo <danesco30@gmail.com>
2018-11-23 15:18:48 -0800

    add game_data seed

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,11 +1,12 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
+  // Deletes ALL existing entries
+  return knex('game_data').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        // Inserts seed entries
+        knex('game_data').insert({id: 1, score: 500, money: 450, pop_points: 50, level: 'level 2', active: true, user_id: 2}),
+        knex('game_data').insert({id: 2, score: 700, money: 550, pop_points: 150, level: 'level 3', active: false, user_id: 3}),
+        knex('game_data').insert({id: 3, score: 200, money: 100, pop_points: 100, level: 'level 1', active: true, user_id: 1})
       ]);
     });
 };
-
