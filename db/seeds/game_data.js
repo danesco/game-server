84d5380ea6324144fcfdcb4fe1082d4fb9dab39f
Dan Escobedo <danesco30@gmail.com>
2018-11-23 15:32:15 -0800

    change pk imn seeds

--- db/seeds/game_data.js
+++ db/seeds/game_data.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('game_data').insert({id: 1, score: 5500, money: 4550, pop_points: 1250, level: 'level 2', active: true, user_id: 2}),
-        knex('game_data').insert({id: 1, score: 1500, money: 5350, pop_points: 5310, level: 'level 2', active: false, user_id: 3}),
-        knex('game_data').insert({id: 1, score: 9500, money: 4250, pop_points: 4050, level: 'level 2', active: true, user_id: 1})
+        knex('game_data').insert({id: 2, score: 1500, money: 5350, pop_points: 5310, level: 'level 2', active: false, user_id: 3}),
+        knex('game_data').insert({id: 3, score: 9500, money: 4250, pop_points: 4050, level: 'level 2', active: true, user_id: 1})
       ]);
     });
 };
