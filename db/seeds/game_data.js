5b261e3e0ad5bfc5a1f5099babaee15cecc34cfa
Dan Escobedo <danesco30@gmail.com>
2018-11-23 15:30:49 -0800

    changed seed data information

--- db/seeds/game_data.js
+++ db/seeds/game_data.js
@@ -1,13 +1,12 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('game_data').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('game_data').insert({id: 1, score: 5500, money: 4550, pop_points: 1250, level: 'level 2', active: true, user_id: 2}),
+        knex('game_data').insert({id: 1, score: 1500, money: 5350, pop_points: 5310, level: 'level 2', active: false, user_id: 3}),
+        knex('game_data').insert({id: 1, score: 9500, money: 4250, pop_points: 4050, level: 'level 2', active: true, user_id: 1})
       ]);
     });
 };
