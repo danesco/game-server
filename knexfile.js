9ac8767577f1607b70790554da6482010c42dd31
Dan Escobedo <danesco30@gmail.com>
2018-11-23 16:41:38 -0800

    troubleshooting

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,8 @@ module.exports = {
     connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
-      max: 10
+      max: 10,
+      requestTimeout: 1
     },
     migrations: {
       directory: './db/migrations',
