36dd7f747a645b2c9adef4a3453215880b16be16
Dan Escobedo <danesco30@gmail.com>
2018-11-23 18:07:18 -0800

    testing routes

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("game_data")
       .then((results) => {
         res.json(results);
     });
