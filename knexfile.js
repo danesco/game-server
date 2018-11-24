4482716941ce7c8cfdc8c8c7e3ca30f1c7f16f82
Dan Escobedo <danesco30@gmail.com>
2018-11-23 16:15:05 -0800

    troubleshooting

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.postgresql-flexible-72419 + '?ssl=true',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
