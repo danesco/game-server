16500ed1c63feb3c0bd51748ee412f52c8b3514c
Dan Escobedo <danesco30@gmail.com>
2018-11-25 14:40:18 -0800

    took away a slash

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(express.static("public"));
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://bajoisland.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://bajoisland.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', true);
   next();
